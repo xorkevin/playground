00d10ab6a51722baf735f8ef39213e287aa64229
Kevin Wang <kevin@xorkevin.com>
2024-05-05 02:02:46 -0700

    Impl code url

--- src/container/jsonnetplayground.tsx
+++ src/container/jsonnetplayground.tsx
@@ -1,4 +1,4 @@
-import {type FC, useEffect, useCallback} from 'react';
+import {type FC, useCallback, useEffect, useRef} from 'react';
 
 import {
   Box,
@@ -9,18 +9,89 @@ import {
 } from '@xorkevin/nuke/component/box';
 import {Button, ButtonGroup} from '@xorkevin/nuke/component/button';
 import {
+  Field,
   Form,
   type FormValue,
+  Input,
+  Label,
   Textarea,
   TextareaResize,
   useForm,
-  Field,
-  Label,
-  Input,
 } from '@xorkevin/nuke/component/form';
 import {TextClasses} from '@xorkevin/nuke/component/text';
+import {type Result, isArray, isNil, isResErr} from '@xorkevin/nuke/computil';
+import {useRoute, useRouter} from '@xorkevin/nuke/router';
 
 import styles from './jsonnetplayground.module.css';
+
+const base64Chars = /\+|\/|=/g;
+const textEncoder = new TextEncoder();
+
+const compress = async (data: string): Promise<Result<string, Error>> => {
+  const stream = new Blob([textEncoder.encode(data)])
+    .stream()
+    .pipeThrough(new CompressionStream('gzip'));
+  const blob = await new Response(stream).blob();
+
+  const u = await new Promise<Result<string, Error>>((resolve) => {
+    const fileReader = new FileReader();
+    fileReader.onerror = () => {
+      resolve({err: fileReader.error ?? new Error('File reader error')});
+    };
+    fileReader.onload = () => {
+      if (typeof fileReader.result !== 'string') {
+        resolve({err: new Error('File reader result is not a string')});
+        return;
+      }
+      const idx = fileReader.result.indexOf(',');
+      if (idx < 0) {
+        resolve({err: new Error('File reader result malformed')});
+        return;
+      }
+      const u = fileReader.result
+        .replaceAll(base64Chars, (s) => {
+          switch (s) {
+            case '+':
+              return '-';
+            case '/':
+              return '_';
+            default:
+              return '';
+          }
+        })
+        .slice(idx + 1);
+      resolve({value: u});
+    };
+    fileReader.readAsDataURL(blob);
+  });
+  return u;
+};
+
+const base64URLChars = /-|_/g;
+
+const decompress = async (data: string): Promise<Result<string, Error>> => {
+  const u = data.replaceAll(base64URLChars, (s) => {
+    switch (s) {
+      case '-':
+        return '+';
+      case '_':
+        return '/';
+      default:
+        return '';
+    }
+  });
+  const res = await fetch(`data:application/octet-stream;base64,${u}`);
+
+  const stream =
+    res.body?.pipeThrough(new DecompressionStream('gzip')) ??
+    new Blob(['[]']).stream();
+  try {
+    const u = await new Response(stream).text();
+    return {value: u};
+  } catch (err) {
+    return {err: new Error('Failed decoding data', {cause: err})};
+  }
+};
 
 const Header = ({share}: {share: () => void}) => (
   <Box padded={BoxPadded.TB} paddedSmall>
@@ -94,11 +165,6 @@ const PlaygroundFile = ({id, rm}: {id: string; rm: (id: string) => void}) => {
   );
 };
 
-type FilesState = {
-  files: string[];
-  [key: string]: FormValue;
-};
-
 const Footer = ({add}: {add: () => void}) => (
   <Box padded={BoxPadded.TB} paddedSmall>
     <ButtonGroup>
@@ -106,6 +172,63 @@ const Footer = ({add}: {add: () => void}) => (
     </ButtonGroup>
   </Box>
 );
+
+type FilesState = {
+  files: string[];
+  [key: string]: FormValue;
+};
+
+const filesStateToArray = (s: FilesState): string[] => {
+  const mainData = s[`${MAIN_FILE_ID}:data`] ?? '';
+  const arr = [typeof mainData === 'string' ? mainData : ''];
+  for (const id of s.files) {
+    const name = s[`${id}:name`] ?? '';
+    const data = s[`${id}:data`] ?? '';
+    arr.push(typeof name === 'string' ? name : '');
+    arr.push(typeof data === 'string' ? data : '');
+  }
+  return arr;
+};
+
+const parseJSONStringArr = (s: string): Result<string[], Error> => {
+  try {
+    const o = JSON.parse(s) as unknown;
+    if (!isArray(o)) {
+      return {err: new Error('File state is malformed')};
+    }
+    const arr = o as unknown[];
+    if (arr.every((v): v is string => typeof v === 'string')) {
+      return {value: arr};
+    }
+    return {err: new Error('File state is malformed')};
+  } catch (err) {
+    return {err: new Error('Failed parsing file state from url', {cause: err})};
+  }
+};
+
+const dataStringToFilesState = (s: string): Result<FilesState, Error> => {
+  const arr = parseJSONStringArr(s);
+  if (isResErr(arr)) {
+    return arr;
+  }
+  if (arr.value.length % 2 !== 1) {
+    return {err: new Error('File state is malformed')};
+  }
+  const files: string[] = [];
+  const state: FilesState = {
+    files,
+    [`${MAIN_FILE_ID}:data`]: arr.value[0],
+  };
+  for (let i = 2; i < arr.value.length; i += 2) {
+    const name = arr.value[i - 1];
+    const data = arr.value[i];
+    const id = crypto.randomUUID() as string;
+    files.push(id);
+    state[`${id}:name`] = name;
+    state[`${id}:data`] = data;
+  }
+  return {value: state};
+};
 
 const initFilesState = (): FilesState => {
   return {
@@ -173,7 +296,79 @@ const JsonnetPlayground: FC = () => {
     [formSetState],
   );
 
-  const share = useCallback(() => {}, [formState]);
+  const unmounted = useRef<AbortSignal | undefined>();
+  useEffect(() => {
+    const controller = new AbortController();
+    unmounted.current = controller.signal;
+    return () => {
+      controller.abort();
+    };
+  }, [unmounted]);
+
+  const route = useRoute();
+  const routeNav = route.navigate;
+  const share = useCallback(() => {
+    void (async () => {
+      const code = await compress(JSON.stringify(filesStateToArray(formState)));
+      if (isResErr(code)) {
+        console.error('Failed compressing data', code.err);
+        return;
+      }
+      if (unmounted.current?.aborted === true) {
+        return;
+      }
+      const params = new URLSearchParams({
+        codev0: code.value,
+      });
+      routeNav(`#${params.toString()}`, true);
+    })();
+  }, [formState, unmounted, routeNav]);
+
+  const router = useRouter();
+  const routerURL = router.url;
+  const once = useRef(false);
+  useEffect(() => {
+    if (once.current) {
+      return;
+    }
+    const controller = new AbortController();
+    let hash = routerURL.hash;
+    if (isNil(hash)) {
+      once.current = true;
+      return;
+    }
+    if (hash.startsWith('#')) {
+      hash = hash.slice(1);
+    }
+    const u = new URLSearchParams(hash);
+    const code = u.get('codev0');
+    if (isNil(code) || code === '') {
+      once.current = true;
+      return;
+    }
+    void (async () => {
+      const data = await decompress(code);
+      if (isResErr(data)) {
+        console.error('Failed decompressing data', data.err);
+        once.current = true;
+        return;
+      }
+      if (controller.signal.aborted) {
+        return;
+      }
+      const s = dataStringToFilesState(data.value);
+      if (isResErr(s)) {
+        console.error('Failed parsing url code', s.err);
+        once.current = true;
+        return;
+      }
+      once.current = true;
+      formSetState(s.value);
+    })();
+    return () => {
+      controller.abort();
+    };
+  }, [once, routerURL, formSetState]);
 
   return (
     <Box padded={BoxPadded.LR} center>
